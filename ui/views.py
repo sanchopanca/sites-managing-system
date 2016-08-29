d04a262972cf7b33b6a0b2aae54284650aed130e
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2016-08-29 21:57:08 +0300

    Add stubs for all views

--- ui/views.py
+++ ui/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ui/index.html')
+
+
+def site_entry(request):
+    return render(request, 'ui/site-entry.html')
+
+
+def summary(request):
+    return render(request, 'ui/summary.html')
+
+
+def summary_average(request):
+    return render(request, 'ui/summary.html')
