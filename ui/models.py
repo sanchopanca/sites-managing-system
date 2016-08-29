f159ecc43d9ba9999b35fdaaa21a5dfe4ed92242
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2016-08-29 21:27:11 +0300

    Add __str__ method to Site model

--- ui/models.py
+++ ui/models.py
@@ -5,3 +5,6 @@ class Site(models.Model):
     date = models.DateField()
     a = models.FloatField()
     b = models.FloatField()
+
+    def __str__(self):
+        return '<date: {}, a: {}, b: {}'.format(self.date, self.a, self.b)
